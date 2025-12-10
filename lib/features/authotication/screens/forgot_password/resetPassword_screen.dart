1c3d583eef6039ba5670cb6fba4192516d61ca5f
malikarjun birajdar <mbirajdar449@gmail.com>
2025-12-10 11:43:52 +0530

    Create new file password reset email send page

--- lib/features/authotication/screens/forgot_password/resetPassword_screen.dart
+++ lib/features/authotication/screens/forgot_password/resetPassword_screen.dart
@@ -1,6 +1,9 @@
 import 'package:e_commerce_app_2/common/style/padding.dart';
+import 'package:e_commerce_app_2/common/widgets/botton/UElevatedBotton.dart';
 import 'package:e_commerce_app_2/features/authotication/screens/login/login_screen.dart';
 import 'package:e_commerce_app_2/utilis/constants/images.dart';
+import 'package:e_commerce_app_2/utilis/constants/size.dart';
+import 'package:e_commerce_app_2/utilis/constants/texts.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -24,8 +27,21 @@ class ResetpasswordScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Image.asset(UImages.mailSentImage)
-              
+              Image.asset(UImages.mailSentImage),
+              SizedBox(height:USizes.spaceBtwItems),
+              Text(UTexts.resetPasswordTitle,style: Theme.of(context).textTheme.headlineMedium,),
+               SizedBox(height:USizes.spaceBtwItems),
+              Text("unknownpro@gmail.com",style: Theme.of(context).textTheme.labelLarge),
+              SizedBox(height:USizes.spaceBtwItems),
+
+
+
+              Text(UTexts.resetPasswordSubTitle,style: Theme.of(context).textTheme.labelMedium,textAlign: TextAlign.center,),
+              SizedBox(height:USizes.spaceBtwSections),
+
+
+              UElevatedBotton(onPressed: (){}, child: Text(UTexts.done)),
+              TextButton(onPressed: (){}, child: Text("Resend Email"))
             ],
           ),
         ),
